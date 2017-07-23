8c90dc85d7c55118d7a659fbc8890277afb88608
Yosuke Hayashi <hayashi.yosuke@gmail.com>
2017-07-23 19:27:15 +0900

    Organize folder structure

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const rollup = require('rollup');
+const node = require('rollup-plugin-node-resolve');
+
+gulp.task('build:d3', async function() {
+  const bundle = await rollup.rollup({
+    entry: 'index.js',
+    plugins: [node()]
+  });
+
+  await bundle.write({
+    format: 'umd',
+    moduleName: 'd3',
+    dest: 'lib/d3.js'
+  })
+});
+
+gulp.task('build:ddt', function() {
+  return gulp.src(['lib/d3.js','src/**/*.js'])
+    .pipe(concat('ddtimelines.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('ddtimelines.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+});
